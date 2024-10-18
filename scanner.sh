48e91d434cc190e8c5cc2facc37def3ea4ba8211
nik-hz <nik0001@protonmail.com>
2024-10-17 21:23:52 -0400

    fixed operator induced panic mode bug

--- scanner.sh
+++ scanner.sh
@@ -5,11 +5,11 @@ echo "Current VIRTUAL_ENV: $CONDA_DEFAULT_ENV"
 if [[ "$CONDA_DEFAULT_ENV" != "" && "$(basename "$CONDA_DEFAULT_ENV")" == "viper" ]]; then
     echo "viper virtual environment is already activated. Running the module..."
     # Run the scanner module directly
-    python3 -m scanner
+    python3 scanner
 else
     echo "viper virtual environment is not activated. Running setup.sh..."
     # Run the setup script
     source ./setup.sh
     # After setup, run the scanner module
-    python3 -m scanner
+    python3 scanner
 fi
